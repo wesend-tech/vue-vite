5b8448ab343d61e079ccb8de45e699701049cf06
shawnnolanjr <shawn@shawnnolan.com>
2024-02-01 10:02:00 -0800

    - removed unused content

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import mix from 'vite-plugin-mix'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
@@ -8,8 +7,5 @@ export default defineConfig( {
 	plugins: [
 		basicSsl(),
 		vue()
-		// mix({
-		//   handler: './apis/handler.ts'
-		// })
 	]
 } )
